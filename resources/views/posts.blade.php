d0b4e41b07addecdc1780b86a13216e6cb0fdc98
Jonathan Rangel <jonathan.rangelrada@gmail.com>
2022-02-20 19:34:55 -0600

    Add app

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/styles.css">
+    <title>My blog</title>
+</head>
+
+<body>
+    <article>
+        <h1><a href="/post/my-first-post">My first post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque delectus illo necessitatibus debitis
+            voluptatibus in eos cumque, veritatis dolores nulla nisi quasi facere, sapiente suscipit impedit
+            repudiandae. A, quis quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis, minus laudantium. Asperiores
+            voluptatem minus dignissimos facere culpa, similique accusantium! Quos perferendis sequi illo similique
+            velit fugit dolorum ullam, hic laboriosam!
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-second-post">My second post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque delectus illo necessitatibus debitis
+            voluptatibus in eos cumque, veritatis dolores nulla nisi quasi facere, sapiente suscipit impedit
+            repudiandae. A, quis quod.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Deleniti fugit perspiciatis odit obcaecati? Iste
+            natus similique reiciendis molestias atque, vitae, sed libero sapiente maxime deserunt, qui amet
+            reprehenderit aspernatur numquam?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores ullam laudantium neque harum fugit. Tenetur
+            quisquam nam laudantium repellendus enim velit hic nostrum animi, iste consectetur rem dolore perferendis
+            assumenda.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post/my-third-post">My third post</a></h1>
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque delectus illo necessitatibus debitis
+            voluptatibus in eos cumque, veritatis dolores nulla nisi quasi facere, sapiente suscipit impedit
+            repudiandae. A, quis quod.
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ab modi incidunt dignissimos accusantium quos,
+            praesentium error possimus, quaerat distinctio ipsa nostrum ex quibusdam porro. Odio obcaecati voluptatum id
+            excepturi!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae nobis placeat dignissimos molestiae excepturi
+            molestias necessitatibus consequatur aut optio, aliquam possimus voluptas quae, enim blanditiis. Illum, unde
+            harum! Optio, quas!
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Praesentium sunt omnis, ducimus eum voluptas
+            corrupti cupiditate ex autem nihil quam soluta aspernatur perspiciatis doloribus aut accusantium? Inventore
+            impedit harum aut?
+        </p>
+    </article>
+</body>
+
+</html>
